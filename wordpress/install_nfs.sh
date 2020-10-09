91a7388f51923b7614f2626307d917c42ec8166e
Augustinas Stirbis <a@cast.ai>
2020-10-09 12:19:14 +0300

    removed wordpress

--- wordpress/install_nfs.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-helm template stable/nfs-server-provisioner --namespace=nfs-provisioner --set=persistence.enabled=true,persistence.storageClass=cast-block-storage,persistence.size=20Gi,storageClass.name=nfs-storage,storageClass.provisionerName=nfs --generate-name
