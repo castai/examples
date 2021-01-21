ea0cfb55f3baddf4dd48374d2f9595f4db34d982
Nate Reid <45366367+natereid72@users.noreply.github.com>
2021-01-21 12:22:24 -0500

    Update install_redis_tables.sh (#5)

--- boutique-eshop-yugabyte/install_redis_tables.sh
+++ boutique-eshop-yugabyte/install_redis_tables.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-kubectl exec -it yb-master-0 -n yugabyte-aws \
+kubectl exec -it yb-master-0 -n yugabyte-aws --container yb-master \
  -- /home/yugabyte/bin/yb-admin \
  --master_addresses "yb-masters.yugabyte-aws.svc.cluster.local:7100,yb-masters.yugabyte-gcp.svc.cluster.local:7100,yb-masters.yugabyte-do.svc.cluster.local:7100" setup_redis_table
